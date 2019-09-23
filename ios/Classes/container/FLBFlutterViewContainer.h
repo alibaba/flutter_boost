48fafd3757db33aea238feeb054659e0b332a166
Leo <stu0615@163.com>
2019-09-23 21:08:53 +0800

    add designated initializer

--- ios/Classes/container/FLBFlutterViewContainer.h
+++ ios/Classes/container/FLBFlutterViewContainer.h
@@ -29,6 +29,7 @@
 NS_ASSUME_NONNULL_BEGIN
 @interface FLBFlutterViewContainer  : FlutterViewController<FLBFlutterContainer>
 @property (nonatomic,copy,readwrite) NSString *name;
+- (instancetype)init NS_DESIGNATED_INITIALIZER;
 - (void)surfaceUpdated:(BOOL)appeared;
 - (void)setEnableForRunnersBatch:(BOOL)enable;
 @end
