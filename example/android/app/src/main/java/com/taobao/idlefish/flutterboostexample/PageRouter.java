dd5de6ac794256409baaa3950371f4988219733f
yangwu.jia <yangwu.jia@taobao.com>
2019-11-13 15:07:50 +0800

    Demo for route result

--- example/android/app/src/main/java/com/taobao/idlefish/flutterboostexample/PageRouter.java
+++ example/android/app/src/main/java/com/taobao/idlefish/flutterboostexample/PageRouter.java
@@ -1,5 +1,6 @@
 package com.taobao.idlefish.flutterboostexample;
 
+import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.util.Log;
@@ -12,11 +13,9 @@ public class PageRouter {
 
     public final static Map<String, String> pageName = new HashMap<String, String>() {{
 
-
         put("first", "first");
         put("second", "second");
         put("tab", "tab");
-
         put("sample://flutterPage", "flutterPage");
     }};
 
@@ -38,21 +37,25 @@ public class PageRouter {
             if (pageName.containsKey(path)) {
                 Intent intent = BoostFlutterActivity.withNewEngine().url(pageName.get(path)).params(params)
                         .backgroundMode(BoostFlutterActivity.BackgroundMode.opaque).build(context);
-
-                context.startActivity(intent);
-
+                if(context instanceof Activity){
+                    Activity activity=(Activity)context;
+                    activity.startActivityForResult(intent,requestCode);
+                }else{
+                    context.startActivity(intent);
+                }
+                return true;
             } else if (url.startsWith(FLUTTER_FRAGMENT_PAGE_URL)) {
                 context.startActivity(new Intent(context, FlutterFragmentPageActivity.class));
                 return true;
             } else if (url.startsWith(NATIVE_PAGE_URL)) {
                 context.startActivity(new Intent(context, NativePageActivity.class));
                 return true;
-            } else {
-                return false;
             }
+
+            return false;
+
         } catch (Throwable t) {
             return false;
         }
-        return false;
     }
 }
