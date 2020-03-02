8cf66572915003217d98d7c426551c1cd341d647
justin <noborder@qq.com>
2020-03-02 20:05:59 +0800

    PluginsRegister 不再对外暴露，需要手动注册插件请使用  FlutterBoost.instance().engineProvider().getPlugins().add(new FlutterPlugin());

--- android/src/main/java/com/idlefish/flutterboost/FlutterBoost.java
+++ android/src/main/java/com/idlefish/flutterboost/FlutterBoost.java
@@ -25,7 +25,6 @@ public class FlutterBoost {
     private FlutterViewContainerManager mManager;
     private FlutterEngine mEngine;
     private Activity mCurrentActiveActivity;
-    private PluginRegistry mRegistry;
     private boolean mEnterActivityCreate =false;
     static FlutterBoost sInstance = null;
 
@@ -64,11 +63,6 @@ public class FlutterBoost {
                 mCurrentActiveActivity = activity;
                 if (mPlatform.whenEngineStart() == ConfigBuilder.ANY_ACTIVITY_CREATED) {
                     doInitialFlutter();
-                    boostPluginRegistry();
-                }
-                if (mPlatform.whenEngineStart() == ConfigBuilder.IMMEDIATELY) {
-                    boostPluginRegistry();
-
                 }
 
             }
@@ -182,17 +176,9 @@ public class FlutterBoost {
         );
 
         flutterEngine.getDartExecutor().executeDartEntrypoint(entrypoint);
-        mRegistry = new BoostPluginRegistry(createEngine());
 
     }
 
-    public void boostPluginRegistry(){
-        if(mRegistry!=null&& !mRegistry.hasPlugin("boostPluginRegistry")){
-            mPlatform.registerPlugins(mRegistry);
-            mRegistry.registrarFor("boostPluginRegistry");
-        }
-
-    }
 
     public static class ConfigBuilder {
 
@@ -323,9 +309,6 @@ public class FlutterBoost {
         return mManager.findContainerById(id);
     }
 
-    public PluginRegistry getPluginRegistry() {
-        return mRegistry;
-    }
 
     private FlutterEngine createEngine() {
         if (mEngine == null) {
@@ -355,7 +338,6 @@ public class FlutterBoost {
             mPlatform.lifecycleListener.onEngineDestroy();
         }
         mEngine = null;
-        mRegistry = null;
         mCurrentActiveActivity = null;
     }
 
@@ -371,10 +353,5 @@ public class FlutterBoost {
         void onEngineDestroy();
     }
 
-
-    public interface BoostPluginsRegister {
-
-        void registerPlugins(PluginRegistry mRegistry);
-    }
 
 }
