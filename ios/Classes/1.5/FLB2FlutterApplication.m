863e83d3783a188d7e3a66337dfd226c444d1c85
Luke <luke.lkj@taobao.com>
2020-04-28 17:28:42 +0800

    删除无用代码

--- ios/Classes/1.5/FLB2FlutterApplication.m
+++ ios/Classes/1.5/FLB2FlutterApplication.m
@@ -213,11 +213,6 @@
 
 - (void)onShownContainerChanged:(NSString *)uniqueId
                          params:(NSDictionary *)params{
-    NSString *oldName = params[@"oldName"];
-    NSString *newName = params[@"newName"];
-    if (oldName!=nil && [newName isEqualToString:@"default"]) {
-        [self.flutterProvider detach];
-    }
 }
 
 @end
