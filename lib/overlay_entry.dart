34433838c621837f3217c4834c32119fb4a19a73
Rulong Chen（陈汝龙） <rulong.crl@alibaba-inc.com>
2021-03-03 19:11:00 +0800

    删除僵尸代码

--- lib/overlay_entry.dart
+++ lib/overlay_entry.dart
@@ -2,25 +2,16 @@ import 'package:flutter/widgets.dart';
 import 'package:flutter_boost/boost_container.dart';
 
 final GlobalKey<OverlayState> overlayKey = GlobalKey<OverlayState>();
-List<_ContainerOverlayEntry> _leastEntries;
-
 void refreshOverlayEntries(List<BoostContainer<dynamic>> containers) {
   final OverlayState overlayState = overlayKey.currentState;
-
   if (overlayState == null) {
     return;
   }
-  if (_leastEntries != null && _leastEntries.isNotEmpty) {
-    for (_ContainerOverlayEntry entry in _leastEntries) {
-      entry.remove();
-    }
-  }
-  _leastEntries = containers
+
+  overlayState.insertAll(containers
       .map<_ContainerOverlayEntry>((BoostContainer<dynamic> container) =>
           _ContainerOverlayEntry(container))
-      .toList(growable: false);
-
-  overlayState.insertAll(_leastEntries);
+      .toList(growable: false));
 }
 
 class _ContainerOverlayEntry extends OverlayEntry {
