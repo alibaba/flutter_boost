5b4ec823b40b17c80b66c23508df57c103c609ac
Rulong Chen（陈汝龙） <rulong.crl@alibaba-inc.com>
2021-03-10 17:34:02 +0800

    Revert "删除僵尸代码"
    This reverts commit 34433838c621837f3217c4834c32119fb4a19a73.

--- lib/overlay_entry.dart
+++ lib/overlay_entry.dart
@@ -2,16 +2,26 @@ import 'package:flutter/widgets.dart';
 import 'package:flutter_boost/boost_container.dart';
 
 final GlobalKey<OverlayState> overlayKey = GlobalKey<OverlayState>();
+List<_ContainerOverlayEntry> _lastEntries;
+
 void refreshOverlayEntries(List<BoostContainer> containers) {
   final OverlayState overlayState = overlayKey.currentState;
   if (overlayState == null) {
     return;
   }
 
-  overlayState.insertAll(containers
-      .map<_ContainerOverlayEntry>((BoostContainer container) =>
-          _ContainerOverlayEntry(container))
-      .toList(growable: false));
+  if (_lastEntries != null && _lastEntries.isNotEmpty) {
+    for (_ContainerOverlayEntry entry in _lastEntries) {
+      entry.remove();
+    }
+  }
+
+  _lastEntries = containers
+      .map<_ContainerOverlayEntry>(
+          (BoostContainer container) => _ContainerOverlayEntry(container))
+      .toList(growable: false);
+
+  overlayState.insertAll(_lastEntries);
 }
 
 class _ContainerOverlayEntry extends OverlayEntry {
