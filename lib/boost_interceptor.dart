25153e9ce0a8cf9e1dc1acd22adb81c885efa18c
Rulong Chen（陈汝龙） <rulong.crl@alibaba-inc.com>
2021-05-08 16:56:36 +0800

    路由拦截接口重构

--- lib/boost_interceptor.dart
+++ lib/boost_interceptor.dart
@@ -1,22 +1,74 @@
-import 'package:flutter_boost/boost_navigator.dart';
+import 'dart:async';
 
-///The Interceptor to intercept the [push] method in [BoostNavigator]
-abstract class BoostInterceptor {
-  ///The callback of BoostInterceptor for push new page method
-  Future<void> onPush(BoostInterceptorOption option);
-}
+import 'package:flutter_boost/boost_navigator.dart';
 
 ///The request object in Interceptor,which is to passed
 class BoostInterceptorOption {
-  BoostInterceptorOption({this.isBlocked, this.name, this.arguments});
-
-  ///Indicates the [BoostInterceptorRequest] is blocked or not.
-  ///If [isBlocked] , the push operation will be blocked and nothing will happen.
-  bool isBlocked = false;
+  BoostInterceptorOption(this.name, this.arguments);
 
   ///your page name in route table
   String name;
 
   ///the arguments you want to pass in next page
   Map<String, dynamic> arguments;
+
+  @override
+  String toString() =>
+      "Instance of 'BoostInterceptorOption'(name:$name, arguments:$arguments)";
+}
+
+enum InterceptorResultType {
+  next,
+  resolve,
+}
+
+class InterceptorState<T> {
+  InterceptorState(this.data, [this.type = InterceptorResultType.next]);
+
+  T data;
+  InterceptorResultType type;
+}
+
+class _BaseHandler {
+  // ignore: always_specify_types
+  final _completer = Completer<InterceptorState>();
+
+  // ignore: always_specify_types
+  Future<InterceptorState> get future => _completer.future;
+
+  bool get isCompleted => _completer.isCompleted;
+}
+
+/// Handler for push interceptor.
+class PushInterceptorHandler extends _BaseHandler {
+  /// Continue to call the next push interceptor.
+  void next(BoostInterceptorOption options) {
+    _completer.complete(InterceptorState<BoostInterceptorOption>(options));
+  }
+
+  /// Return the result directly!
+  /// Other request interceptor(s) will not be executed.
+  ///
+  /// [result]: Response object to return.
+  void resolve(Object result) {
+    _completer.complete(
+      InterceptorState<Object>(
+        result,
+        InterceptorResultType.resolve,
+      ),
+    );
+  }
+}
+
+///The Interceptor to intercept the [push] method in [BoostNavigator]
+class BoostInterceptor {
+  /// The callback will be executed before the push is initiated.
+  ///
+  /// If you want to continue the push, call [handler.next].
+  ///
+  /// If you want to complete the push with some custom data，
+  /// you can resolve a [result] object with [handler.resolve].
+  ///
+  void onPush(BoostInterceptorOption option, PushInterceptorHandler handler) =>
+      handler.next(option);
 }
