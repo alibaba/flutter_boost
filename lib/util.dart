aabf84367fd061fb6fea2ce07421ea519ec40a1b
MiracleSoul <max.miracle.soul@gmail.com>
2021-05-18 16:47:51 +0800

    修改工具类

--- lib/util.dart
+++ lib/util.dart
@@ -4,13 +4,13 @@ import 'flutter_boost_app.dart';
 
 class BoostUtil {
   ///
-  /// try to get route uniqueId.
-  /// If routeSetting is BoostPage, uniqueId will be return.
+  /// try to get route info.
+  /// If routeSetting is BoostPage, will be return.
   /// Else return null.
-  static String tryGetRouteUniqueId(Route<dynamic> route) {
+  static BoostPage<dynamic> tryGetRouteInfo(Route<dynamic> route) {
     final RouteSettings routeSettings = route.settings;
     if (routeSettings is BoostPage<dynamic>){
-      return routeSettings.pageInfo.uniqueId;
+      return routeSettings;
     }
     return null;
   }
