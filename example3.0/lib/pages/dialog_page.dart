67610609aba69872b9ec52d2f2d50c1c8d242faa
luckysmg <2539699336@qq.com>
2021-05-25 17:26:51 +0800

    提交全新的example（针对3.0）

---
+++ example3.0/lib/pages/dialog_page.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_boost/flutter_boost.dart';
+
+class DialogPage extends StatefulWidget {
+  const DialogPage({Key key}) : super(key: key);
+
+  @override
+  _DialogPageState createState() => _DialogPageState();
+}
+
+class _DialogPageState extends State<DialogPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Align(
+      alignment: Alignment.bottomCenter,
+      child: Container(
+        height: 250,
+        alignment: Alignment.center,
+        width: MediaQuery.of(context).size.width,
+        decoration: const BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.only(
+                topLeft: Radius.circular(30),
+                topRight: const Radius.circular(30))),
+        child: Material(
+          child: CupertinoButton(
+              color: Colors.deepPurpleAccent,
+              child: Text('点击退出弹窗'),
+              onPressed: () {
+                //这里退出依然要用Boost的导航器，而不是官方的Navigator
+                BoostNavigator.instance.pop();
+              }),
+        ),
+      ),
+    );
+  }
+}
