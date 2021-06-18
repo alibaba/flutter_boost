210f12a36d18a9d4c4606c899dc4475196ba3a5f
Rulong Chen（陈汝龙） <rulong.crl@alibaba-inc.com>
2021-06-18 15:06:15 +0800

    增加webview插件测试案例

--- example/lib/flutter_page.dart
+++ example/lib/flutter_page.dart
@@ -214,6 +214,20 @@ class _FlutterRouteWidgetState extends State<FlutterRouteWidget>
                     margin: const EdgeInsets.all(8.0),
                     color: Colors.yellow,
                     child: Text(
+                      'WebView Example',
+                      style: TextStyle(fontSize: 22.0, color: Colors.black),
+                    )),
+                onTap: () => BoostNavigator.instance
+                    .push("webview", withContainer: true)
+                    .then((value) =>
+                        print('xlog, webview, Return Value:$value')),
+              ),
+              InkWell(
+                child: Container(
+                    padding: const EdgeInsets.all(8.0),
+                    margin: const EdgeInsets.all(8.0),
+                    color: Colors.yellow,
+                    child: Text(
                       'push flutter widget',
                       style: TextStyle(fontSize: 22.0, color: Colors.black),
                     )),
