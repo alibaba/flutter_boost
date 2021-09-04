26c9961673267cee44ffb3181ecebfb54cb4467f
wenjingrui.wjr <wenjingrui.wjr@alibaba-inc.com>
2021-09-04 23:28:46 +0800

    更新example_new注释，未修改框架任何代码

--- example_new/lib/main.dart
+++ example_new/lib/main.dart
@@ -18,8 +18,7 @@ void main() {
 }
 
 ///创建一个自定义的Binding，继承和with的关系如下，里面什么都不用写
-class CustomFlutterBinding extends WidgetsFlutterBinding
-    with BoostFlutterBinding {}
+class CustomFlutterBinding extends WidgetsFlutterBinding with BoostFlutterBinding {}
 
 class MyApp extends StatefulWidget {
   @override
@@ -27,17 +26,19 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
+  /// 由于很多同学说没有跳转动画，这里是因为之前exmaple里面用的是 [PageRouteBuilder]，
+  /// 其实这里是可以自定义的，和Boost没太多关系，比如我想用类似iOS平台的动画，
+  /// 那么只需要像下面这样写成 [CupertinoPageRoute] 即可
+  /// (这里全写成[MaterialPageRoute]也行，这里只不过用[CupertinoPageRoute]举例子)
+  ///
+  /// 注意，如果需要push的时候，两个页面都需要动的话，
+  /// （就是像iOS native那样，在push的时候，前面一个页面也会向左推一段距离）
+  /// 那么前后两个页面都必须是遵循CupertinoRouteTransitionMixin的路由
+  /// 简单来说，就两个页面都是CupertinoPageRoute就好
+  /// 如果用MaterialPageRoute的话同理
+
   static Map<String, FlutterBoostRouteFactory> routerMap = {
-    ///flutter主功能展示页面
     'mainPage': (settings, uniqueId) {
-      /// 由于很多同学说没有跳转动画，这里是因为之前exmaple里面用的是PageRouteBuilder，
-      /// 其实这里是可以自定义的，和Boost没太多关系，比如我想用类似IOS平台的动画，
-      /// 那么只需要像下面这样写成CupertinoPageRoute即可
-      ///
-      /// 注意，如果需要push的时候，两个页面都需要动的话，
-      /// （就是像iOS native那样，在push的时候，前面一个页面也会向左推一段距离）
-      /// 那么前后两个页面都必须是遵循CupertinoRouteTransitionMixin的路由
-      /// 简单来说，就两个页面都是CupertinoPageRoute就好
       return CupertinoPageRoute(
           settings: settings,
           builder: (_) {
