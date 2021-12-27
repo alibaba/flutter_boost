50bb23904fcfc57cd415afd19a439ea2138be423
郭翰林 <2318560278@qq.com>
2021-12-27 17:45:45 +0800

    feat: Android抛出popRoute代理回调 (#1531)

--- example/android/app/src/main/java/com/idlefish/flutterboost/example/MyFlutterBoostDelegate.java
+++ example/android/app/src/main/java/com/idlefish/flutterboost/example/MyFlutterBoostDelegate.java
@@ -1,6 +1,7 @@
 package com.idlefish.flutterboost.example;
 
 import android.content.Intent;
+import android.widget.Toast;
 
 import com.idlefish.flutterboost.FlutterBoost;
 import com.idlefish.flutterboost.FlutterBoostDelegate;
@@ -28,5 +29,12 @@ public class MyFlutterBoostDelegate implements FlutterBoostDelegate {
                 .urlParams(options.arguments())
                 .build(FlutterBoost.instance().currentActivity());
         FlutterBoost.instance().currentActivity().startActivity(intent);
+    }
+
+    @Override
+    public boolean popRoute(FlutterBoostRouteOptions options) {
+        //自定义popRoute处理逻辑,如果不想走默认处理逻辑返回true进行拦截
+        Toast.makeText(FlutterBoost.instance().currentActivity(), "自定义popRoute处理逻辑", Toast.LENGTH_SHORT).show();
+        return false;
     }
 }
